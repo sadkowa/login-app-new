c7026fab0408887ee0e2c0843bf2fc026b13027a
Pawelkaniak <pawkaniak@gmail.com>
2024-02-05 12:58:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
 app.use(cors());
 app.use(express.json());
 
@@ -9,36 +10,35 @@ const userBody = {
   userName: 'tester',
   fullUserName: 'Archie Karas',
 };
+
 const apiToken = {
   token: '90917fs91-1041-4491-oe66',
 };
 
 app.post('/login', (req, res) => {
-  const reqB = req.body;
-  const { email, userName } = userBody;
+  const { userName, password } = req.body;
   try {
-    if (!reqB.userName || !reqB.password) {
-      return res.status(400).send(`Wrong data`);
+    if (!userName || !password) {
+      return res.status(400).json({ message: 'Missing username or password' });
     }
-    reqB.email === email &&
-    reqB.userName === userName &&
-    reqB.password === 'test1234'
-      ? res.status(200).json(apiToken)
-      : res.status(401).json('Wrong credentials');
+    if (userName === userBody.userName && password === 'test1234') {
+      res.status(200).json(apiToken);
+    } else {
+      res.status(401).json({ message: 'Wrong credentials' });
+    }
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ message: 'An error occurred', error: err.message });
   }
 });
 
 app.get('/authUser', (req, res) => {
-  const reqH = req.headers;
   try {
-    if (reqH.authorization !== apiToken.token) {
-      res.status(401).json('Wrong auth apiToken');
+    if (req.headers.authorization !== apiToken.token) {
+      return res.status(401).json({ message: 'Wrong auth token' });
     }
-    return res.status(200).json(userBody);
+    res.status(200).json(userBody);
   } catch (err) {
-    console.log(err);
+    res.status(500).json({ message: 'An error occurred', error: err.message });
   }
 });
 
