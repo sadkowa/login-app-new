8bd943490beed5348eb1d3236909c7064394b1c6
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-06 11:44:04 +0100

    Create LoginAuthApi

---
+++ src/api/LoginAuthApi.js
@@ -0,0 +1,42 @@
+import { loginApi, authUserApi } from "."
+
+class LoginAuthApi {
+
+    login() {
+        return fetch(loginApi, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: 'candidate@test.com',
+                userName: 'tester',
+                password: 'test1234',
+            })
+        })
+            .then(this.handleErrors)
+            .then(resp => resp.json())
+    }
+
+    getFullName(token) {
+        return fetch(authUserApi, {
+            method: "Get",
+            headers: {
+                "Authorization": token
+            },
+        })
+            .then(this.handleErrors)
+            .then(resp => resp.json())
+    }
+
+    handleErrors(resp) {
+        if (!resp.ok) {
+            return resp.json()
+                .then(errorData => {
+                    // Promise.reject(new Error(errorData.message))
+                    throw new Error(errorData.message)
+                })
+        } return resp
+    }
+
+}
+
+export default LoginAuthApi
