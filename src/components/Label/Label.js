919b70c0700f43a1acf63d810ac06a43c5cd1e3e
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-06 14:11:51 +0100

    Create Label with styles

---
+++ src/components/Label/Label.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StyledLabel from "./Label.styled";
+
+const Label = ({children}) => {
+    return (
+        <StyledLabel>
+            {children}
+        </StyledLabel>
+    )
+}
+
+export default Label
---
+++ src/components/Label/Label.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledLabel = styled.label`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 10px;
+    font-size: 1rem
+`
+
+export default StyledLabel
