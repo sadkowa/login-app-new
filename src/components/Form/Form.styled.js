f374df040f2afae5d310a352b92584b4f274a80d
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-06 14:12:21 +0100

    Edit styles for Form

--- src/components/Form/Form.styled.js
+++ src/components/Form/Form.styled.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const StyledForm = styled.form`
     display: flex;
     flex-direction: column;
+    width: 50%;
+    margin: 50px auto
 `
 
 export default StyledForm
