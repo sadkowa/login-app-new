0ddc36441226d1b0defdcd47bdcb3460fee6919e
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-06 17:39:13 +0100

    Add props in FormField

--- src/components/FormField/FormField.js
+++ src/components/FormField/FormField.js
@@ -1,9 +1,15 @@
 import React from "react";
 import StyledFormField from "./FormField.styled";
 
-const FormField = ({ children, type }) => {
+const FormField = ({ children, type, name, value, onChange, onBlur }) => {
     return (
-        <StyledFormField type={type}>
+        <StyledFormField
+            value={value}
+            type={type}
+            name={name}
+            onChange={onChange}
+            onBlur={onBlur}
+        >
             {children}
         </StyledFormField>
     )
