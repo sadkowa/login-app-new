4d86a43226de0148cce9d9295bfd47c93ffe696d
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-06 13:25:36 +0100

    Add Form to App

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,65 @@ import '../App.css';
 import React, { useState } from 'react';
 import LoginAuthApi from '../api/LoginAuthApi';
 
+import { initUserData, userDataFields } from '../helpers/formData';
+
 import { Header } from './Header';
+import { Form } from './Form';
 
 function App() {
+  const [currentStepIndex, setCurrentStepIndex] = useState(1)
+
+  const [userData, setUserData] = useState(initUserData)
+
+  // const [token, setToken] = useState('')
+  // const [error, setError] = useState('')
+  // const loginApi = new LoginAuthApi()
+
+  // loginApi.login()
+  //   .then(data => setToken(data))
+  //   .catch(error => {
+  //     console.log(error)
+  //     // setError(error)
+  //   }
+  //     )
+  // .finally(()=> {
+  //   loginApi.getFullName()
+
+
+  const renderFields = () => {
+    return userDataFields.map(field => {
+      const { id, stepId, type, name, label } = field
+
+      if (stepId === currentStepIndex) {
+        return (
+          <label key={id}>{label}
+            <input
+              type={type}
+              name={name}
+            />
+          </label>
+        )
+      }
+    })
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+    console.log('clicked')
+  }
 
   return (
     <div className='App'>
-      {/* <header className='App-header'>Login App</header> */}
       <Header>Login App</Header>
+      <Form onSubmit={submitHandler}>
+        {renderFields()}
+        {currentStepIndex === 0 && (
+          <input type='submit' value="next" />
+        )}
+        {currentStepIndex === 1 && (
+          <input type='submit' value="submit" />
+        )}
+      </Form>
     </div>
   );
 }
