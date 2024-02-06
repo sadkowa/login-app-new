104cf6ac49a09a042b565e009658248735ca7cf2
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-06 17:40:54 +0100

    Implement form validation

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,40 @@ import '../App.css';
 import React, { useState } from 'react';
 import LoginAuthApi from '../api/LoginAuthApi';
 
-import { initUserData, userDataFields } from '../helpers/formData';
+import { initUserData, userDataFields, fieldValidate } from '../helpers/formData';
 
 import { Header } from './Header';
 import { Form } from './Form';
 import { Label } from './Label';
 import { FormField } from './FormField';
 import { SubmitInput } from './SubmitInput';
+import { Button } from './Button';
+import { Error } from './Error';
 
 function App() {
   const [currentStepIndex, setCurrentStepIndex] = useState(0)
 
   const [userData, setUserData] = useState(initUserData)
+  const [errors, setErrors] = useState({})
 
-  // const [token, setToken] = useState('')
-  // const [error, setError] = useState('')
-  // const loginApi = new LoginAuthApi()
+  const [token, setToken] = useState('')
+  const [error, setError] = useState('')
+  const loginApi = new LoginAuthApi()
 
-  // loginApi.login()
-  //   .then(data => setToken(data))
-  //   .catch(error => {
-  //     console.log(error)
-  //     // setError(error)
-  //   }
-  //     )
-  // .finally(()=> {
-  //   loginApi.getFullName()
 
+  const changeHandler = (e, field) => {
+    const { name, value } = e.target
+    setUserData({ ...userData, [name]: value })
+    console.log(userData)
+  }
+
+  const blurHandler = (e, field) => {
+    const { name } = field
+    const currentErrorMessage = fieldValidate(field, userData)
+
+    setErrors({ ...errors, [name]: [currentErrorMessage] })
+    console.log(errors)
+  }
 
   const renderFields = () => {
     return userDataFields.map(field => {
@@ -39,18 +46,29 @@ function App() {
         return (
           <Label key={id}>{label}
             <FormField
+              value={userData[name]}
               type={type}
               name={name}
+              onChange={e => changeHandler(e, field)}
+              onBlur={e => blurHandler(e, field)}
             />
+            {errors[name] && <Error>{errors[name]}</Error>}
           </Label>
         )
       }
     })
   }
 
+  const clickNextButton = () => {
+    console.log(userData.email)
+  }
+
   const submitHandler = e => {
     e.preventDefault()
-    console.log(e)
+
+    if (currentStepIndex === 0) {
+
+    }
   }
 
   return (
@@ -60,7 +78,8 @@ function App() {
         {renderFields()}
         {currentStepIndex === 0 && (
           <Label>
-            <SubmitInput type='submit' value="next" />
+            <Button onClick={clickNextButton}>Next</Button>
+            {/* <SubmitInput type='submit' value="next" /> */}
           </Label>
         )}
         {currentStepIndex === 1 && (
