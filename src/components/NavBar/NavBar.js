c4e01f13dee9b099f072dc05c05c58ed94d77269
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-07 18:41:38 +0100

    Create NavBar with styles

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StyledNavBar from "./NavBar.styled";
+
+const NavBar = ({ children }) => {
+
+    return (
+        <StyledNavBar>
+            {children}
+        </StyledNavBar>
+    )
+}
+
+export default NavBar
---
+++ src/components/NavBar/NavBar.styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const StyledNavBar = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+    margin: 10px 30px 20px;
+    gap: 10px;
+    height: 100%;
+
+    @media (min-width:650px) {
+        position: absolute;
+        display: flex;
+        align-items: center;
+        width: 220px;
+        margin: 0; 
+        height: 100%;
+        top: 0;
+        right:20px;
+    }
+
+    @media (min-width: 1200px) {
+        margin-right: 50px;
+    }
+`
+
+export default StyledNavBar
