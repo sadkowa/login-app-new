d044e8963b8e6e63e8f4d5b640d7a8aef2219cd0
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-07 20:17:51 +0100

    Add props destructuring

--- src/components/Form/Form.styled.js
+++ src/components/Form/Form.styled.js
@@ -3,9 +3,12 @@ import styled from "styled-components";
 const StyledForm = styled.form`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    /* align-items: center; */
+    flex-grow: 1;
     width: 50%;
     max-width: 400px;
-    margin: 50px auto
+    margin: 0 auto 
 `
 
 export default StyledForm
--- src/components/UserPanel/UserPanel.styled.js
+++ src/components/UserPanel/UserPanel.styled.js
@@ -1,22 +1,29 @@
 import styled from "styled-components";
 
-const StyledUserPanel = styled.div`
-    width: 100%;
-    margin-top: 60px;
+const StyledUserPanel = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
 
     & h2 {
+        margin: 20px 10px 30px;
         font-size: 1.8rem;
-        line-height: 8rem;
-        color: white; 
     }
 
     & h3 {
         font-size: 1.5rem;
-        line-height: 3rem;
+        line-height: 2rem;
 
     }
     & p {
         color: green
+    }
+
+    & img {
+        max-width: 70%;
+        margin: 60px;
     }
 `
 
