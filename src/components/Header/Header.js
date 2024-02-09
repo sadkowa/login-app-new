61175cd1dcdaf74ddaadd19eefc4d9a1388fce06
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-09 18:06:17 +0100

    Revert "Create StyledHeader"
    This reverts commit c60db44fedfb2cd2fc6d23e480788f0b8f17b6c8.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,11 @@ import StyledHeader from "./Header.styled";
 
 const Header = ({children}) => {
 
-    return <StyledHeader>{children}</StyledHeader>
+    return (
+        <StyledHeader>
+            {children}
+        </StyledHeader>
+    )
 }
 
 export default Header
